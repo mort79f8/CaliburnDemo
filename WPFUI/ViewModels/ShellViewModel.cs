1c32e7e0b0391d398f8d854dae40efe068ded018
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-27 18:40:46 +0100

    Add project files.

---
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+		private string _firstName = "Tim";
+		private string _lastName;
+		private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+		private PersonModel _selectedPerson;
+
+		public ShellViewModel()
+		{
+			People.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+			People.Add(new PersonModel { FirstName = "Bill", LastName = "Jones" });
+			People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+		}
+
+		public string FirstName
+		{
+			get 
+			{ 
+				return _firstName; 
+			}
+			set 
+			{ 
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string LastName
+		{
+			get 
+			{ 
+				return _lastName; 
+			}
+			set 
+			{ 
+				_lastName = value;
+				NotifyOfPropertyChange(() => LastName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string FullName
+		{
+			get { return $"{ FirstName } {LastName}"; }
+		}
+
+
+		public BindableCollection<PersonModel> People
+		{
+			get { return _people; }
+			set { _people = value; }
+		}
+
+		public PersonModel SelectedPerson
+		{
+			get { return _selectedPerson; }
+			set 
+			{ 
+				_selectedPerson = value;
+				NotifyOfPropertyChange(() => SelectedPerson);
+			}
+		}
+
+		// one way to do the bool (not as readable as the method below):
+		//public bool CanClearText(string firstName, string lastName) => !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+		public bool CanClearText(string firstName, string lastName)
+		{
+			//one liner of the code below (again not a clear as the code below)
+			//return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+
+			if (String.IsNullOrWhiteSpace(firstName) && String.IsNullOrWhiteSpace(lastName))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		public void ClearText(string firstName, string lastName)
+		{
+			FirstName = "";
+			LastName = "";
+		}
+
+		public void LoadPageOne()
+		{
+			ActivateItem(new FirstChildViewModel());
+		}
+
+		public void LoadPageTwo()
+		{
+			ActivateItem(new SecondChildViewModel());
+		}
+	}
+}
